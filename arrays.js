e3faebf9cae0bf09bab585f15f0fb7c3a094bb93
ruthuparnairavally <ruthu199425@gmail.com>
2025-03-10 06:00:27 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+const fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+console.log(fruits);
+
+const numbers1 = new Array(1, 2, 3, 4, 5);
+console.log(numbers1);
